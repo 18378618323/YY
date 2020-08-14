2523ab40f5819ca68682636748e19404b260ef0e
yao <3326857083@qq.com>
2020-08-14 20:28:01 +0800

    sdfs

--- ccc/src/ccc/Test.java
+++ ccc/src/ccc/Test.java
@@ -3,8 +3,7 @@ package ccc;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("好久好久好久结婚呵呵好久好久");
-		System.out.println("好久好久好久结婚呵呵好久好久");
-		System.out.println("好久好久好久结婚呵呵好久好久");
+		
 		System.out.println("好久好久好久结婚呵呵好久好久");
 		System.out.println("好久好久好久结婚呵呵好久好久");
 	}
